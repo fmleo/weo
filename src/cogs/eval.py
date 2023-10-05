16b8ec59f9dfd40db56079218cfff22b46809664
leo <fmleonardo@protonmail.com>
2023-10-05 20:49:19 -0300

    fix(eval): escape mentions

--- src/cogs/eval.py
+++ src/cogs/eval.py
@@ -2,6 +2,8 @@ import asyncio
 from discord.ext import commands
 import discord
 
+from discord.utils import escape_markdown, escape_mentions
+
 from RestrictedPython import compile_restricted
 
 from cogs.utils import restricted_globals
@@ -13,9 +15,9 @@ def get_code_output(code: str) -> str:
         locals_dict = {}
         exec(bytecode, restricted_globals, locals_dict)
         # await ctx.reply(f"```py\n{code}```")
-        return f"`{discord.utils.escape_markdown(locals_dict.get('result'))}`"
+        return f"`{escape_mentions(escape_markdown(locals_dict.get('result')))}`"
     except Exception as e:
-        return f"nuh uh: `{discord.utils.escape_markdown(str(e))}`"
+        return f"nuh uh: `{escape_mentions(escape_markdown(str(e)))}`"
 
 
 class Eval(commands.Cog):
